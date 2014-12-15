723f50b09ce6be7098fc36c762b5462e1f6b8c8b
mstaylor <mills.staylor@siriusxm.com>
2014-12-15 16:32:04 -0500

    updates for public to private subnet mapping using boto and instance metadata

--- gateway-grabber.py
+++ gateway-grabber.py
@@ -1,11 +1,10 @@
 #
 # Gateway Grabber - 2014-02-27
 # joe@uberboxen.net
-#
+# bud.staylor@gmail.com
 # Repoints the default gw for a routing table to the current instance
 # that is running this script.
 #
-# Set the CFN_ROUTETABLE env var with your CloudFormation template
 #
 
 import boto
@@ -18,27 +17,76 @@ dry_run = False
 # AWS access/secret keys (None if using EC2 role)
 aws_access      = None
 aws_secret      = None
-region_name     = 'us-east-1'
+subnet_id = None
+vpc_conn = None
 
-try:
-    cfn_routetable = os.environ['CFN_ROUTETABLE']
-except:
-    print "CFN_ROUTETABLE environment variable is not set!"
-    sys.exit(1)
 
 try:
     instance_id = boto.utils.get_instance_metadata()['instance-id']
+
+    if instance_id is None:
+        print "Did not receive an instance id from isntmetadata"
+        sys.exit(1)
+    print "instance id is %s\n" % instance_id
+
+    az = boto.utils.get_instance_metadata()['placement']['availability-zone']
+    if az is None:
+        print "Did not receive an availability zone from metadata"
+        sys.exit(1)
+    print "Instance Availability Zone is %s\n" % az
+
+    mac = boto.utils.get_instance_metadata()['mac']
+    if mac is None:
+        print "Did not receive a mac address from instance metadata"
+        sys.exit(1)
+    print "Instance mac address is %s\n" % mac
+
+    current_subnet_id = boto.utils.get_instance_metadata()['network']['interfaces']['macs'][mac]['subnet-id']
+    if current_subnet_id is None:
+        print "Did not receive a subnet id from instance metadata"
+        sys.exit(1)
+    print "current subnet id for instance is %s" % current_subnet_id
+
+    vpc_id = boto.utils.get_instance_metadata()['network']['interfaces']['macs'][mac]['vpc-id']
+    if vpc_id is None:
+        print "Did not receive a vpc id from instance metadata"
+        sys.exit(1)
+    print "current vpc id for instance is %s" % vpc_id
+    vpc_conn = boto.connect_vpc(aws_access_key_id=aws_access, aws_secret_access_key=aws_secret)
+    subnets = vpc_conn.get_all_subnets(filters={'availabilityZone': az, 'vpcId': vpc_id})
+    for subnet in subnets:
+        print "current subnet to check is %s" % subnet.id
+        if subnet.id != current_subnet_id:
+            subnet_id = subnet.id
+            break
+
+    if subnet_id is None:
+        print "Did not find a second subnet"
+        sys.exit(1)
+
+    print "subnet_id is %s" % subnet_id
+
 except:
     print "Could not get EC2 instance ID!"
     sys.exit(1)
 
-vpc_conn = boto.connect_vpc(aws_access_key_id=aws_access, aws_secret_access_key=aws_secret)
 ec2_conn = boto.connect_ec2(aws_access_key_id=aws_access, aws_secret_access_key=aws_secret)
+final_route = None
 
 try:
-    rt = vpc_conn.get_all_route_tables(route_table_ids=os.environ['CFN_ROUTETABLE'])[0]
+    routes = vpc_conn.get_all_route_tables(filters={'association.subnet-id': subnet_id, 'association.main': 'false'})
+    for rt in routes:
+        final_route = rt
+        gw_route = next((route for route in rt.routes if route.destination_cidr_block == '0.0.0.0/0' and route.gateway_id is not None), None)
+        if gw_route is not None:
+            final_route = None
+
 except Exception, e:
-    print "Could not find route table [%s]: %s" % (os.environ['CFN_ROUTETABLE'], e)
+    print "Could not find route table [%s]: %s" % (subnet_id, e)
+    sys.exit(1)
+
+if final_route is None:
+    print "Could not find route table [%s]" % (subnet_id)
     sys.exit(1)
 
 print "Found the route table: %s" % (rt.id,)
@@ -55,22 +103,21 @@ if source_dest_check:
         print "Could not modify source/dest check: %s" % (e,)
         sys.exit(1)
 
-gw_route = next((route for route in rt.routes if route.destination_cidr_block == '0.0.0.0/0'), None)
+gw_route = next((route for route in final_route.routes if route.destination_cidr_block == '0.0.0.0/0'), None)
 if not gw_route:
     print "Could not find default gw route in routing table!"
 else:
-    print "Found a gateway route: %s, %s, %s" % (rt.id, gw_route.destination_cidr_block, instance_id)
+    print "Found a gateway route: %s, %s, %s" % (final_route.id, gw_route.destination_cidr_block, instance_id)
     try:
-        vpc_conn.delete_route(rt.id, '0.0.0.0/0', dry_run=dry_run)
+        vpc_conn.delete_route(final_route.id, '0.0.0.0/0', dry_run=dry_run)
     except Exception, e:
         print "Could not delete gw route! %s" % (e,)
         sys.exit(1)
 
 try:
-    vpc_conn.create_route(rt.id, '0.0.0.0/0', instance_id=instance_id, dry_run=dry_run)
+    vpc_conn.create_route(final_route.id, '0.0.0.0/0', instance_id=instance_id, dry_run=dry_run)
 except Exception, e:
     print "Could not replace gw route! %s" % (e,)
     sys.exit(1)
 
-print "Gateway grabbed!"
-
+print "Route table updated!"
